a048c6bae5c3312ca7f3f7accd21bc6cad9312fd
hua(Kungfu) <84950880+kungful@users.noreply.github.com>
2025-04-19 22:26:56 +0800

    Update hua_nodes.py

--- hua_nodes.py
+++ hua_nodes.py
@@ -11,6 +11,10 @@ from PIL import Image, ImageOps, ImageSequence, ImageFile
 from PIL.PngImagePlugin import PngInfo
 import folder_paths  # 假设这是一个自定义模块，用于处理文件路径
 from comfy.cli_args import args
+import comfy.utils # Need this import for Hua_LoraLoader
+import node_helpers # Need this import for GradioInputImage
+import torch # Need this import for GradioInputImage
+from datetime import datetime # Need this import for Hua_Output
 
 
 OUTPUT_DIR = folder_paths.get_output_directory()
@@ -34,6 +38,31 @@ class Hua_gradio_Seed:
     def hua_seed(seed):
         show_help = "https://github.com/kungful/ComfyUI_hua_boy.git"
         return (seed, show_help,)
+#---------------------------------------------------------------------------------------------------------------------#
+class Hua_gradio_resolution:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "custom_width": ("INT", {"default": 512, "min": 64, "max": 8192, "step": 64}),
+                "custom_height": ("INT", {"default": 512, "min": 64, "max": 8192, "step": 64}),
+            }
+        }
+
+    RETURN_TYPES = ("INT", "INT",)
+    RETURN_NAMES = ("width", "height",)
+    FUNCTION = "get_resolutions"
+
+    CATEGORY = icons.get("hua_boy_one")
+
+    def get_resolutions(self, custom_width, custom_height):
+        width, height = custom_width, custom_height
+
+
+        return (width, height)
 #---------------------------------------------------------------------------------------------------------------------#
 
 class Hua_gradio_jsonsave:
@@ -77,17 +106,122 @@ class Hua_gradio_jsonsave:
 
             # Save JSON file
             filename_with_batch_num = f"{filename_prefix}"
-            
+
             json_filename = f"{filename_with_batch_num}_{counter:05}_.json"
             json_data = prompt
             json_file_path = os.path.join(full_output_folder, json_filename)
             with open(json_file_path, 'w', encoding='utf-8') as json_file:
                 json.dump(json_data, json_file, ensure_ascii=False, indent=4)
-            
+
             # 调试信息0+
             print(f"保存的api格式json文件位置: {json_file_path}")
             counter += 1
-           
+
 
 
         return { "ui": { "images": results } }
+
+
+class Hua_LoraLoader:
+    def __init__(self):
+        self.loaded_lora = None
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model": ("MODEL", {"tooltip": "The diffusion model the LoRA will be applied to."}),
+                "clip": ("CLIP", {"tooltip": "The CLIP model the LoRA will be applied to."}),
+                "lora_name": (folder_paths.get_filename_list("loras"), {"tooltip": "The name of the LoRA."}),
+                "strength_model": ("FLOAT", {"default": 1.0, "min": -100.0, "max": 100.0, "step": 0.01, "tooltip": "How strongly to modify the diffusion model. This value can be negative."}),
+                "strength_clip": ("FLOAT", {"default": 1.0, "min": -100.0, "max": 100.0, "step": 0.01, "tooltip": "How strongly to modify the CLIP model. This value can be negative."}),
+            }
+        }
+
+    RETURN_TYPES = ("MODEL", "CLIP")
+    OUTPUT_TOOLTIPS = ("The modified diffusion model.", "The modified CLIP model.")
+    FUNCTION = "load_lora"
+
+    CATEGORY = icons.get("hua_boy_one")
+    DESCRIPTION = "LoRAs are used to modify diffusion and CLIP models, altering the way in which latents are denoised such as applying styles. Multiple LoRA nodes can be linked together."
+
+    def load_lora(self, model, clip, lora_name, strength_model, strength_clip):
+        if strength_model == 0 and strength_clip == 0:
+            return (model, clip)
+
+        lora_path = folder_paths.get_full_path_or_raise("loras", lora_name)
+        lora = None
+        if self.loaded_lora is not None:
+            if self.loaded_lora[0] == lora_path:
+                lora = self.loaded_lora[1]
+            else:
+                self.loaded_lora = None
+
+        if lora is None:
+            lora = comfy.utils.load_torch_file(lora_path, safe_load=True)
+            self.loaded_lora = (lora_path, lora)
+
+        model_lora, clip_lora = comfy.sd.load_lora_for_models(model, clip, lora, strength_model, strength_clip)
+        return (model_lora, clip_lora)
+
+
+class Hua_LoraLoaderModelOnly(Hua_LoraLoader):
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "model": ("MODEL",),
+                              "lora_name": (folder_paths.get_filename_list("loras"), ),
+                              "strength_model": ("FLOAT", {"default": 1.0, "min": -100.0, "max": 100.0, "step": 0.01}),
+                              }}
+    RETURN_TYPES = ("MODEL",)
+    FUNCTION = "load_lora_model_only"
+    CATEGORY = icons.get("hua_boy_one")
+    def load_lora_model_only(self, model, lora_name, strength_model):
+        return (self.load_lora(model, None, lora_name, strength_model, 0)[0],)
+
+
+class Hua_CheckpointLoaderSimple:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "ckpt_name": (folder_paths.get_filename_list("checkpoints"), {"tooltip": "The name of the checkpoint (model) to load."}),
+            }
+        }
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE")
+    OUTPUT_TOOLTIPS = ("The model used for denoising latents.",
+                       "The CLIP model used for encoding text prompts.",
+                       "The VAE model used for encoding and decoding images to and from latent space.")
+    FUNCTION = "load_checkpoint"
+
+    CATEGORY = icons.get("hua_boy_one")
+    DESCRIPTION = "Loads a diffusion model checkpoint, diffusion models are used to denoise latents."
+
+    def load_checkpoint(self, ckpt_name):
+        ckpt_path = folder_paths.get_full_path_or_raise("checkpoints", ckpt_name)
+        out = comfy.sd.load_checkpoint_guess_config(ckpt_path, output_vae=True, output_clip=True, embedding_directory=folder_paths.get_folder_paths("embeddings"))
+        return out[:3]
+
+class Hua_UNETLoader:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "unet_name": (folder_paths.get_filename_list("diffusion_models"), ),
+                              "weight_dtype": (["default", "fp8_e4m3fn", "fp8_e4m3fn_fast", "fp8_e5m2"],)
+                             }}
+    RETURN_TYPES = ("MODEL",)
+    FUNCTION = "load_unet"
+
+    CATEGORY = icons.get("hua_boy_one")
+
+    def load_unet(self, unet_name, weight_dtype):
+        model_options = {}
+        if weight_dtype == "fp8_e4m3fn":
+            model_options["dtype"] = torch.float8_e4m3fn
+        elif weight_dtype == "fp8_e4m3fn_fast":
+            model_options["dtype"] = torch.float8_e4m3fn
+            model_options["fp8_optimizations"] = True
+        elif weight_dtype == "fp8_e5m2":
+            model_options["dtype"] = torch.float8_e5m2
+
+        unet_path = folder_paths.get_full_path_or_raise("diffusion_models", unet_name)
+        model = comfy.sd.load_diffusion_model(unet_path, model_options=model_options)
+        return (model,)
